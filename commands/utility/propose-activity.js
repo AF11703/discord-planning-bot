9f184ad85f2a5280289f1aa7c1959839288cb556
Andres Fraga <aafraga17@gmail.com>
2025-08-05 17:57:07 -0400

    finished final date collection... need to integrate google cal

--- commands/utility/propose-activity.js
+++ commands/utility/propose-activity.js
@@ -20,20 +20,20 @@ module.exports = {
                 .setDescription('Name of activity to plan')
                 .setRequired(true)
         )
-        .addIntegerOption(option => 
+        .addIntegerOption(option =>
             option.setName('response-duration')
                 .setDescription('OPTIONAL | Time for users to respond to plan in minutes')
                 .setRequired(false)
         )
     ,
-    
+
     /**
      * 
      * @param { ChatInputCommandInteraction } interaction 
      */
-    async execute(interaction) {    
+    async execute(interaction) {
         await interaction.deferReply()
-        
+
 
         const dates = []
         for (let addDays = 0; addDays <= 7; addDays++) {
@@ -80,139 +80,180 @@ module.exports = {
                     .setLabel(dates[6])
                     .setValue(dates[6]),
             )
-            
-        
+
+
         const row = new ActionRowBuilder()
             .addComponents(select)
-        
+
         //Looks ugly, but just transforms user input into ms or uses one minute in ms
         //i.e. if user puts two mins => 120_000 ms.. if user puts 0 or less, automatically default to one minute
-        const responseDuration = 
-            interaction.options.getInteger('response-duration') > 0 ? interaction.options.getInteger('response-duration') * ONE_MIN_IN_MS : ONE_MIN_IN_MS 
+        const responseDuration =
+            interaction.options.getInteger('response-duration') > 0 ? interaction.options.getInteger('response-duration') * ONE_MIN_IN_MS : ONE_MIN_IN_MS
         const activity = interaction.options.getString('plan-name')
 
         const response = await interaction.editReply({
             content: `${interaction.user.tag} wants to plan: ${activity}\n
-                You have ${responseDuration/ONE_MIN_IN_MS} minute(s) to respond.`,
+                You have ${responseDuration / ONE_MIN_IN_MS} minute(s) to respond.`,
             components: [row]
         })
 
         const userResponses = new Collection();
-        
+
         const responseCollection = response.createMessageComponentCollector({
             componentType: ComponentType.StringSelect,
             time: responseDuration
         })
 
-        responseCollection.on('collect', async (selection) => {
-            const userId = selection.user.id
-            const userTag = selection.user.tag
-            const userDates = selection.values
-            const timeResponded = new Date()
+        const getFinalOption = new Promise((resolve) => {
+            responseCollection.on('collect', async (selection) => {
+                const userId = selection.user.id
+                const userTag = selection.user.tag
+                const userDates = selection.values
+                const timeResponded = new Date()
 
-            userResponses.set(userId, {
-                tag: userTag,   
-                dates: userDates,
-                response_time: timeResponded
-            })
-            
-            await selection.reply({
-                content: `Response recorded... You chose dates:\n ${userDates.join('\n')} on ${format(timeResponded, 'Pp')}`,
-                flags: MessageFlags.Ephemeral,
-                components: []
-            })
-        })
-
-
-        responseCollection.on('end', async () => {
-            const datesCollection = new Collection()
-
-            dates.forEach((date) => {
-                datesCollection.set(date, {
-                    count: 0,
-                    users: []
+                userResponses.set(userId, {
+                    tag: userTag,
+                    dates: userDates,
+                    response_time: timeResponded
+                })
+                await selection.reply({
+                    content: `Response recorded... You chose date(s):\n ${userDates.join('\n')} on ${format(timeResponded, 'Pp')}`,
+                    flags: MessageFlags.Ephemeral,
+                    components: []
                 })
             })
 
-            for (const [userId, userData] of userResponses) {
-                userData.dates.forEach((date) => {
-                    const data = datesCollection.get(date)
-                    if (data) {
-                        data.count++
-                        data.users.push(userId)
-                    }
-                })
-            }
 
-            
-            const dateOptions = [...datesCollection.filter((data) => data.count > 0)
-                .sort((dataA, dataB) => dataB.count - dataA.count)]
+            responseCollection.on('end', async () => {
+                const datesCollection = new Collection()
 
-
-            if (!dateOptions) {
-                await interaction.editReply({
-                    content: 'No response recorded to the /plan command. Please try again.',
-                    flags: MessageFlags.Ephemeral
+                dates.forEach((date) => {
+                    datesCollection.set(date, {
+                        count: 0,
+                        users: []
+                    })
                 })
 
-                return
-            }
+                for (const [userId, userData] of userResponses) {
+                    userData.dates.forEach((date) => {
+                        const data = datesCollection.get(date)
+                        if (data) {
+                            data.count++
+                            data.users.push(userId)
+                        }
+                    })
+                }
 
 
-            const maxOptions = dateOptions.filter((data) => data.count === dateOptions[0].count)
+                const dateOptions = [...datesCollection.filter((data) => data.count > 0)
+                    .sort((dataA, dataB) => dataB.count - dataA.count)]
 
-            if (maxOptions.length == 1) {
-                app.listen(3000, () => {
-                    console.log("Callback URL clicked")
-                })
 
-                //TODO: Setup Google Calendar event build
-                const calendarBtn = new ButtonBuilder()
-                    .setLabel('Google Calendar')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('http://localhost:3000/api')
-                const btnRow = new ActionRowBuilder()
-                    .addComponents(calendarBtn)
-                await interaction.editReply({
-                    content: `${activity} has been planned for ${maxOptions[0][0]}.\n 
-                    Click below to add event to Google Calendar`,
-                    components: [btnRow]
-                })
-            }
-            else {
-                const finalizeDateSelect = new StringSelectMenuBuilder()
-                    .setCustomId('maxDate-select')
-                    .setPlaceholder('Finalize date selection')
-                    .setMinValues(1)
-                    .setMaxValues(1)
-                    .addOptions(
-                        maxOptions.map((obj) => new StringSelectMenuOptionBuilder()
-                            .setLabel(obj[0])
-                            .setValue(obj[0])
+                if (!dateOptions) {
+                    await interaction.editReply({
+                        content: 'No response recorded to the /plan command. Please try again.',
+                        flags: MessageFlags.Ephemeral,
+                        components: []
+                    })
+
+                    resolve(null)
+                    return
+                }
+
+
+                const maxOptions = dateOptions.filter((data) => data.count === dateOptions[0].count)
+
+                if (maxOptions.length > 1) {
+                    const finalizeDateSelect = new StringSelectMenuBuilder()
+                        .setCustomId('maxDate-select')
+                        .setPlaceholder('Finalize date selection')
+                        .setMinValues(1)
+                        .setMaxValues(1)
+                        .addOptions(
+                            maxOptions.map((obj) => new StringSelectMenuOptionBuilder()
+                                .setLabel(obj[0])
+                                .setValue(obj[0])
+                            )
                         )
-                    )
-                
-                const finalSelectRow = new ActionRowBuilder()
-                    .addComponents(finalizeDateSelect)
-                const finalDateResponse = await interaction.editReply({
-                    content: 'Choose the date to finalize planning',
-                    components: [finalSelectRow]
-                })
-                
-                //TODO: Continue response collection for final date selection
-                const finalDateCollection = finalDateResponse.createMessageComponentCollector()
 
-                finalDateCollection.on('collect', (selection) => {
-                    console.log('collected')
-                })
-            }
+                    const finalSelectRow = new ActionRowBuilder()
+                        .addComponents(finalizeDateSelect)
+                    const finalDateResponse = await interaction.editReply({
+                        content: `${userMention(interaction.user.id)}, choose a date to finalize planning`,
+                        components: [finalSelectRow],
+                        flags: MessageFlags.Ephemeral
+                    })
+
+                    
+                    const finalDateCollection = finalDateResponse.createMessageComponentCollector({
+                        componentType: ComponentType.StringSelect,
+                        time: responseDuration
+                    })
+                    const maxDateResponses = new Collection()
+
+                    finalDateCollection.on('collect', async (selection) => {
+                        const userId = selection.user.id
+                        const userTag = selection.user.tag
+                        const userDate = selection.values
+                        const timeResponded = new Date()
+
+                        maxDateResponses.set(userId, {
+                            tag: userTag,
+                            dates: userDate,
+                            response_time: timeResponded
+                        })
+
+                        await selection.reply({
+                            content: `Response recorded... You chose:\n ${userDate} on ${format(timeResponded, 'Pp')}`,
+                            flags: MessageFlags.Ephemeral,
+                            components: []
+                        })
+                    })
+
+
+                    finalDateCollection.on('end', async () => {
+                        const maxDatesCollection = new Collection()
+
+                        dates.forEach((date) => {
+                            maxDatesCollection.set(date, {
+                                count: 0,
+                                users: []
+                            })
+                        })
+
+                        for (const [userId, userData] of maxDateResponses) {
+                            userData.dates.forEach((date) => {
+                                const data = maxDatesCollection.get(date)
+                                if (data) {
+                                    data.count++
+                                    data.users.push(userId)
+                                }
+                            })
+                        }
+
+                        const finalDate = (maxDatesCollection.filter((data) => data.count > 0)
+                            .sort((dataA, dataB) => dataB.count - dataA.count)).firstKey()
+                        resolve(finalDate)
+                    })
+
+                }
+                else {
+                    resolve(maxOptions[0][0])
+                }
+            })
         })
 
-        
+        const finalOption = await getFinalOption
+
+        //TODO: Use below to add event to GCal
+        if(finalOption) {
+            await interaction.followUp({
+                content: `${activity} has been planned for ${finalOption}.`
+            })
+        }
 
 
-        
-        
-        
+
+
     }
 }
